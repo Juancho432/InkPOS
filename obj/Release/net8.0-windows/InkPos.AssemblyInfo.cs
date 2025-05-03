ec0e9dc2695d8914eb241edd6e243d697ba4b1b3
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2025-05-02 20:33:48 -0500

    Correccion de .gitignore

--- obj/Release/net8.0-windows/InkPos.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/InkPos.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("InkPos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a928a601a5d16cd1813d0b49a685a0f61925cd52")]
 [assembly: System.Reflection.AssemblyProductAttribute("InkPos")]
