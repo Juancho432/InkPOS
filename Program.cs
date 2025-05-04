d76bfb0ca98dc96f70fac0ac15aab74590ef2a6f
Emmanuel Mora <emmanuel.mora388@pascualbravo.edu.co>
2025-05-04 15:43:47 -0500

    EXCELENTE....SISTEMA POS DE 5git add .git add .git add .git add .git add .!

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace InkPos
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
-            Application.Run(new Login());
+            Application.Run(new busqueda_productos());
         }
     }
 
