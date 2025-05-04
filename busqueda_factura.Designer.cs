6419c6a8f95b33f682e4dc00bbee75feb9b47f64
Sebastian Echeverri <sebastian.echeverri602@pascualbravo.edu.co>
2025-05-04 10:11:24 -0500

    instalación librería guna

---
+++ busqueda_factura.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace InkPos
+{
+    partial class busqueda_factura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1487, 723);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(487, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(456, 48);
+            label1.TabIndex = 0;
+            label1.Text = "Búsqueda de facturas";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(615, 220);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(185, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // busqueda_factura
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Thistle;
+            ClientSize = new Size(1523, 747);
+            Controls.Add(groupBox1);
+            Name = "busqueda_factura";
+            Text = "InkPOS - Búsquda de facturas";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
