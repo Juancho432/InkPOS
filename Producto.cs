59585f6233362b01c93207e11245e3ebb954bf1a
Pablo <juan.florez756@pascualbravo.edu.co>
2025-05-11 14:49:27 -0500

    se corrigeron las clases

--- Producto.cs
+++ Producto.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace InkPos
 {
-    public class Producto(string IdProd, string NameItem, int stock)
+    public class Producto(string IdProd, string NameItem,decimal Precio, int stock)
     {
         public string IdProducto { get; set; } = IdProd;
         public string NombreItem { get; set; } = NameItem;
+        public decimal Precio { get; set; } = Precio;
         public int Stock { get; set; } = stock;
      
 
